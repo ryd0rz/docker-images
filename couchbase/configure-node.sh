1d257c49c4bf37bf4aff445d687be6e9a7624658
Alex Merrifield <alex.merrifield@levelupdevelopment.com>
2019-10-18 06:07:45 -0400

    update couchbase version and add travel data in

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -17,10 +17,10 @@ curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1q
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 
 # Setup Memory Optimized Indexes
-curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=forestdb'
 
 # Load travel-sample bucket
-#curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
 
 echo "Type: $TYPE"
 
